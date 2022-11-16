9901200079fefb7b70e3de7b69cf8b3de4e0f78e
PuriPuri <gregory.weston@list.ru>
2022-11-16 23:45:08 +0300

    task1

---
+++ task1/src/task1.java
@@ -0,0 +1,14 @@
+public class task1 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+        int i = 0;
+
+        if ((n > 0) & (m > 0)) {
+            do {
+                System.out.print((i % n) + 1);
+                i = i + (m - 1);
+            } while ((i % n) != 0);
+        }
+    }
+}
